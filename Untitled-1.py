65053cdc2f3fe591a5bc38bc1e4c87a99d1a07d6
Sofiabene <128842174+Sofiabene@users.noreply.github.com>
2023-08-30 13:48:30 -0300

    qualquer coisa

---
+++ Untitled-1.py
@@ -0,0 +1,19 @@
+altura= float(input("digite sua altura:"))
+peso= float(input("digite seu peso:"))
+
+print("resultado do imc:")
+imc =peso/ (altura * altura)
+print (imc)
+
+if(imc < 18.5):
+    print("abaixo do peso")
+elif(imc < 25):
+    print("peso normal")
+elif(imc < 30):
+    print("sobrepeso")
+elif(imc < 35):
+    print("obesidade grau 1")
+elif(imc < 40):
+    print("obesidade grau 2")
+else:
+    print("obesidade grau 3")
